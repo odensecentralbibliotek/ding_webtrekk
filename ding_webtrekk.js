3feffa366e443e37e8d9ddd18114ad55e879c4b1
yusuf amer <impara1@gmail.com>
2021-03-03 11:21:26 +0100

    DDB updates

--- ding_webtrekk.js
+++ ding_webtrekk.js
@@ -10,7 +10,6 @@
  */
 
 (function($) {
-
   "use strict";
 
   // Webtrekk tracking parameters.
@@ -21,53 +20,51 @@
   var DING_WEBTREKK_PARAMETER_CAROUSEL_NEXT = 59;
   var DING_WEBTREKK_PARAMETER_CAROUSEL_PREV = 60;
 
-  var appendQueryParameter = function(url, key, value) {
+  var appendQueryParameter = function appendQueryParameter(url, key, value) {
     var seperator = (url.indexOf('?') !== -1) ? '&' : '?';
     var urlParameter = key + '=' + encodeURIComponent(value);
     return url + seperator + urlParameter;
   };
 
-  var pushEvent = function(event, eventData) {
+  var pushEvent = function pushEvent(event, eventData) {
+    if (Drupal.settings.dingWebtrekk.debugMode) {
+      console.log('Ding webtrekk - Pushing ' + event + ' event with data: ',
+        JSON.stringify(eventData, null, 2)
+      );
+    }
     // Ensure that the Webtrekk object is defined before pushing event.
     if (typeof wts !== 'undefined') {
       wts.push(['send', event, eventData]);
     }
   };
 
+  // Enables tracking without cookies. This method uses IP + user agent
+  // fingerprinting instead to keep sessions together. Cross session analysis
+  // will no longer be possible.
+  // TODO: Insert link to documentation when available. As of writing this is
+  // very new stuff.
+  var noCookieTracking = function noCookieTracking() {
+    if (typeof wts !== 'undefined') {
+      wts.push(['setIdentifierOptOut']);
+    }
+  }
+
   // Other modules dealing with client side webtrekk tracking might find these
   // functions useful, so make them available on the global Drupal object.
   Drupal.dingWebtrekkAppendQueryParameter = appendQueryParameter;
   Drupal.dingWebtrekkPushEvent = pushEvent;
+  Drupal.dingWebtrekkNoCookieTracking= noCookieTracking;
 
   Drupal.behaviors.ding_webtrekk = {
     attach: function(context) {
       // Send Webtrekk events attached in the backend.
-      $('.js-ding-webtrekk-event', context).once('js-ding-webtrekk').click(function() {
+      $('.js-ding-webtrekk-event', context)
+        .once('js-ding-webtrekk')
+        .click(function() {
         var eventData = $(this).data('ding-webtrekk-event');
         pushEvent('click', eventData);
-      });
-
-      // Secial handling for ding entity rating event.
-      //
-      // It would require a complete rework of the ding_entity_rating_display
-      // theme hook to avoid this. It doesn't provide the ability to add custom
-      // attributes/classes in preprocess and is not using using render arrays
-      // for rating elements, making them problematic to modify.
-      $('.js-ding-webtrekk-rating-event', context).once('js-ding-webtrekk', function() {
-        var contentId = $(this).data('ding-entity-rating-id');
-        $('.js-rating-symbol', this).each(function(index) {
-          var rating = (index + 1) + '';
-          $(this).click(function() {
-            var eventData = {
-              linkId: 'Materiale rating',
-              customClickParameter: {}
-            };
-            eventData.customClickParameter[DING_WEBTREKK_PARAMETER_RENEW_RATING] = rating;
-            eventData.customClickParameter[DING_WEBTREKK_PARAMETER_RENEW_RATING_ID] = contentId;
-            pushEvent('click', eventData);
-          });
-        });
-      });
+        }
+      );
 
       // Track autocomplete selections.
       $('.js-ding-webtrekk-autocomplete .form-autocomplete', context)
@@ -81,7 +78,8 @@
             eventData.customClickParameter[DING_WEBTREKK_PARAMETER_AUTOCOMPLETE] = $(selected).text();
             pushEvent('click', eventData);
           }
-      });
+        }
+      );
 
       // Track ding loan renew selected events.
       //
@@ -119,7 +117,7 @@
       // to pass as value in event and URL-parameter. For tabbed carousels the
       // title of the individuals carousels in the tabs are set in the
       // data-title attribute of the carousels. For single carousels the title
-      // is not avialable here, but instead it's controlled by the configuration
+      // is not available here, but instead it's controlled by the configuration
       // on the panel pane. There are no panel hooks where we can get the title
       // and easily change the content tree to add event and URL-parameters to
       // elements (content is already rendered when hook is run).
@@ -155,7 +153,35 @@
           pushEvent('click', eventData);
         });
       });
+
+      // Track loaded ding_campaign_plus campaigns.
+      //
+      // Sometimes campaigns are loaded asynchronously if they are not in cache.
+      // In these cases we can't use page parameters, since we don't have the
+      // required information at initial page load and we'll have to use an
+      // event. To be consistent we track every campaign with event.
+      $('.node-ding-campaign-plus', context) .once('js-ding-webtrekk', function() {
+        var eventData = $(this).data('ding-webtrekk-event');
+        // Sending a 'click'-event to webtrekk in this case may seem weird, but
+        // currently our Webtrekk setup only supports this type of event. If our
+        // setup is changed in the future it may be more appropriate to send
+        // another type of event. The important thing now is that we get the
+        // data to Webtrekk.
+        pushEvent('click', eventData);
+      });
     }
   };
+
+  // EU cookie compliance integration.
+  if (typeof Drupal.eu_cookie_compliance !== 'undefined') {
+    var method = Drupal.settings.eu_cookie_compliance.method;
+    // When using opt-in enable no-cookie tracking if user hasn't agreed.
+    // When using opt-out enable no-cookie tracking if user has disagreed. Since
+    // there's no method for that, we check for the value explicitly.
+    if ((method === 'opt_in' && !Drupal.eu_cookie_compliance.hasAgreed())
+        || (method === 'opt_out' && Drupal.eu_cookie_compliance.getCurrentStatus() === 0)) {
+      noCookieTracking();
+    }
+  }
 
 })(jQuery);
